cc89d6ee92f67803bc891ddb54100cbb60331831
ahps <akbarputrasrg@unimed.ac.id>
2024-07-13 07:01:51 +0700

    feat: implement file upload with full path, not only relative path

--- main.go
+++ main.go
@@ -22,12 +22,18 @@ type RequestConfig struct {
 }
 
 func getFile(filePath string) (string, []byte) {
-	execDir, err := os.Getwd()
-	if err != nil {
-		panic(err)
+	fullPath := filePath
+
+	// If file path is relative, get the full path
+	if strings.HasPrefix(filePath, "./") {
+		execDir, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+
+		fullPath = path.Join(execDir, filePath)
 	}
 
-	fullPath := path.Join(execDir, filePath)
 	file, err := os.ReadFile(fullPath)
 	if err != nil {
 		panic(err)
