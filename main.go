2faa117f86af65533f3e041a41fd37be7955dca1
ahps <akbarputrasrg@unimed.ac.id>
2024-09-22 10:37:46 +0700

    refactor: make main.go more concise

--- main.go
+++ main.go
@@ -35,8 +35,8 @@ func run(configs []RequestConfig) {
 			buffer, ctype = parseJSONBody(&config, responses)
 		}
 
-		fullUrl := parseUrl(&config, responses)
-		request, err := http.NewRequest(config.Method, fullUrl, buffer)
+		url := parseUrl(&config, responses)
+		request, err := http.NewRequest(config.Method, url, buffer)
 		if err != nil {
 			panic(err)
 		}
@@ -49,41 +49,29 @@ func run(configs []RequestConfig) {
 		}
 
 		timestamp := time.Now()
-		client := &http.Client{}
-		response, err := client.Do(request)
+		response, err := (&http.Client{}).Do(request)
 		if err != nil {
 			panic(err)
 		}
 
 		body := response.Body
-		responseBody, err := io.ReadAll(body)
+		bodyBuffer, err := io.ReadAll(body)
 		if err != nil {
 			panic(err)
 		}
 
-		err = body.Close()
-		if err != nil {
+		if err = body.Close(); err != nil {
 			panic(err)
 		}
 
-		var responseJSON interface{}
-		err = json.Unmarshal(responseBody, &responseJSON)
-		if err != nil {
-			responseJSON = "\"" + string(responseBody) + "\""
+		var bodyStr interface{}
+		if err = json.Unmarshal(bodyBuffer, &bodyStr); err != nil {
+			bodyStr = "\"" + string(bodyBuffer) + "\""
 			next = false
 		}
 
 		headers := make(map[string]interface{})
 		for k, v := range response.Header {
-			if len(v) > 1 {
-				value := ""
-				for _, item := range v {
-					value += item + ";"
-				}
-				headers[k] = strings.TrimSuffix(value, ";")
-				continue
-			}
-
 			isJSON := strings.HasPrefix(v[0], "{") || strings.HasPrefix(v[0], "[")
 			if !isJSON || !json.Valid([]byte(v[0])) {
 				headers[k] = v[0]
@@ -98,19 +86,19 @@ func run(configs []RequestConfig) {
 			headers[k] = m
 		}
 
-		pretty, err := json.MarshalIndent(map[string]interface{}{
-			"_____url":    fullUrl,
+		jsonStr, err := json.MarshalIndent(map[string]interface{}{
+			"_____url":    url,
 			"____status":  response.Status,
 			"___duration": time.Since(timestamp).String(),
 			"__headers":   headers,
-			"_body":       responseJSON,
+			"_body":       bodyStr,
 		}, "", "  ")
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("%s", pretty)
 
-		// break early if status code is not 200
+		fmt.Printf("%s", jsonStr)
+
 		if !next || response.StatusCode != 200 {
 			break
 		}
@@ -119,7 +107,7 @@ func run(configs []RequestConfig) {
 			fmt.Println(",\n\n")
 		}
 
-		responses[i] = responseJSON
+		responses[i] = bodyStr
 	}
 	fmt.Print("]")
 }
